22822052fda772c13960e358ba7d6c222fc00543
Brian L. Gorman <blgorman@gmail.com>
2023-01-11 20:43:57 -0600

    initial project creation

---
+++ GITBisectAtTheMovies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GITBisectAtTheMovies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
