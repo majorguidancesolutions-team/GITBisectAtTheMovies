1c3e22381918b12f1625b1cb680f5c1fa6cab4db
Brian L. Gorman <blgorman@gmail.com>
2023-01-11 21:22:42 -0600

    able to display data from the API

--- GITBisectAtTheMovies/Models/MovieData.cs
+++ GITBisectAtTheMovies/Models/MovieData.cs
@@ -1,24 +1,57 @@
-﻿namespace GITBisectAtTheMovies.Models
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GITBisectAtTheMovies.Models
 {
     public class MovieData
     {
-        public List<Movie> Movies { get; set; } = new List<Movie>();
-        public MovieData()
+        private static readonly HttpClient _client;
+
+        static MovieData()
         {
-            GetMovies();
+            _client = new HttpClient();
         }
 
-        private void GetMovies() 
+        public static async Task<List<Movie>> GetMovies(string apiKey, string apiURL, string apiTop250, bool useAPI)
         {
-
-            if (Movies == null || !Movies.Any())
+            if (string.IsNullOrWhiteSpace(apiKey) || string.IsNullOrEmpty(apiURL) || string.IsNullOrWhiteSpace(apiTop250))
             {
-                Movies.Add(new Movie() { 
-                    id = "1", crew = "Sam Neill, Jeff Goldblum, Laura Dern"
-                    , fullTitle = "Jurassic Park (1993)", image = "", imDbRating = "9.4", imDbRatingCount = "10000"
-                    , rank = "25", title = "Jurassic Park", year = "1993"
-                });
+                throw new ArgumentException("API Key and/or API URL or API route not set correctly");
             }
+
+            var movies = new List<Movie>();
+
+            if (useAPI)
+            {
+                
+                _client.BaseAddress = new Uri($"{apiURL}{apiTop250}{apiKey}");
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var movieData = await _client.GetStringAsync("");
+                var allMovies = JsonSerializer.Deserialize<AllMovies>(movieData);
+                if (allMovies?.items != null && allMovies.items.Any())
+                {
+                    movies = allMovies.items;
+                }
+            }
+            
+
+            if (movies.Count == 0)
+            {
+                movies.Add(new Movie()
+                {
+                    id = "tt0111161",
+                    rank = "1",
+                    title = "The Shawshank Redemption",
+                    fullTitle = "The Shawshank Redemption (1994)",
+                    year = "1994",
+                    image = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.6716_AL_.jpg",
+                    crew = "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
+                    imDbRating = "9.2",
+                    imDbRatingCount = "2683217"
+                });
+
+            }
+            return movies;
         }
     }
 }
