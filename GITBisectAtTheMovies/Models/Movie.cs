acaa9deeec5876f537919e9908c7e5928159050a
Brian L. Gorman <blgorman@gmail.com>
2023-01-11 20:48:11 -0600

    added initial movie model

---
+++ GITBisectAtTheMovies/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace GITBisectAtTheMovies.Models
+{
+    public class Movie
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string MPAARating { get; set; }
+        public int Year { get; set; }
+    }
+}
