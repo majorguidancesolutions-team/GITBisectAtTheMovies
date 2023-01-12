e1e8104e9f32d15a36bce00b61d5d2368c339e08
Brian L. Gorman <blgorman@gmail.com>
2023-01-11 21:06:47 -0600

    reworked movie

--- GITBisectAtTheMovies/Models/Movie.cs
+++ GITBisectAtTheMovies/Models/Movie.cs
@@ -2,10 +2,14 @@
 {
     public class Movie
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string MPAARating { get; set; }
-        public int Year { get; set; }
+        public string id { get; set; }
+        public string rank { get; set; }
+        public string title { get; set; }
+        public string fullTitle { get; set; }
+        public string year { get; set; }
+        public string image { get; set; }
+        public string crew { get; set; }
+        public string imDbRating { get; set; }
+        public string imDbRatingCount { get; set; }
     }
 }
