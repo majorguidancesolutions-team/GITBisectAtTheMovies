19a982159dbc528a9229fa37e46b58eddda6328c
Brian L. Gorman <blgorman@gmail.com>
2023-01-11 20:51:40 -0600

    Get Movies Mock Data

---
+++ GITBisectAtTheMovies/Models/MovieData.cs
@@ -0,0 +1,22 @@
+﻿namespace GITBisectAtTheMovies.Models
+{
+    public class MovieData
+    {
+        public List<Movie> Movies { get; set; } = new List<Movie>();
+        public MovieData()
+        {
+            GetMovies();
+        }
+
+        private void GetMovies() 
+        {
+
+            if (Movies == null || !Movies.Any())
+            {
+                Movies.Add(new Movie() { 
+                    Id = 1, Description = "Dinosaurs", MPAARating = "PG-13", Title = "Jurassic Park", Year = 1993
+                });
+            }
+        }
+    }
+}
